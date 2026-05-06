ac51b745107b4e34793cea27088b9f46f8a3ac23
ertibeqiri1 <eb70067@ubt-uni.net>
2026-05-06 14:01:11 +0200

    add city search UI to weather drawer

--- lib/views/widgets/weather_drawer.dart
+++ lib/views/widgets/weather_drawer.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../core/app_colors.dart';
+import '../../viewmodels/weather_viewmodel.dart';
 
-class WeatherDrawer extends StatelessWidget {
+class WeatherDrawer extends StatefulWidget {
   const WeatherDrawer({super.key});
+
+  @override
+  State<WeatherDrawer> createState() => _WeatherDrawerState();
+}
+
+class _WeatherDrawerState extends State<WeatherDrawer> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _onSearch(BuildContext context) {
+    context.read<WeatherViewModel>().searchCity(_searchController.text);
+    _searchController.clear();
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +40,11 @@ class WeatherDrawer extends StatelessWidget {
         ),
         child: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               _buildHeader(context),
+              const SizedBox(height: 32),
+              _buildSearchSection(context),
             ],
           ),
         ),
@@ -49,6 +73,82 @@ class WeatherDrawer extends StatelessWidget {
               color: AppColors.textPrimary,
               size: 26,
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchSection(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Search City',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: AppColors.textPrimary,
+            ),
+          ),
+          const SizedBox(height: 12),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  style: const TextStyle(color: AppColors.textPrimary),
+                  textInputAction: TextInputAction.search,
+                  onSubmitted: (_) => _onSearch(context),
+                  decoration: InputDecoration(
+                    hintText: 'Enter city name...',
+                    hintStyle: const TextStyle(color: AppColors.textMuted),
+                    filled: true,
+                    fillColor: AppColors.inputBackground,
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: AppColors.textMuted,
+                      size: 20,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.inputBorder),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.inputBorder),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.textSecondary),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              GestureDetector(
+                onTap: () => _onSearch(context),
+                child: Container(
+                  padding: const EdgeInsets.all(13),
+                  decoration: BoxDecoration(
+                    color: AppColors.inputBackground,
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: AppColors.inputBorder),
+                  ),
+                  child: const Icon(
+                    Icons.arrow_forward,
+                    color: AppColors.textPrimary,
+                    size: 22,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
